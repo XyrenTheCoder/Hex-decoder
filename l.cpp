c2ce3077f1a8006304fc9bcf9e8ce41aa00967a4
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-20 17:49:10 +0800

    Update l.cpp

--- l.cpp
+++ l.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     string hex;
     std::cin << hex;
     int len = hex.length();
     std::string newString;
-    for(int i=0; i< len; i+=2) {
+    for(int i=0; i< len; i+=2)
+    {
         std::string byte = hex.substr(i,2);
         char chr = (char) (int)strtol(byte.c_str(), null, 16);
         newString.push_back(chr);
     }
+    return 0;
 }
