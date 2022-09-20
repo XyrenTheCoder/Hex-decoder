452c279a5e4f4d8e055efc1ee2031a2e8ab2ff60
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-20 17:45:09 +0800

    Create l.cpp

---
+++ l.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int len = hex.length();
+    std::string newString;
+    for(int i=0; i< len; i+=2) {
+        std::string byte = hex.substr(i,2);
+        char chr = (char) (int)strtol(byte.c_str(), null, 16);
+        newString.push_back(chr);
+    }
+}
