5da832a61646f4bca9c80362e56c1df253ebc1c5
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-20 17:58:21 +0800

    Update l.cpp

--- l.cpp
+++ l.cpp
@@ -2,15 +2,16 @@
 
 int main()
 {
-    string hex;
-    std::cin << hex;
+    std::string hex;
+    std::cin >> hex;
     int len = hex.length();
-    std::string newString;
+    std::string o;
     for(int i=0; i< len; i+=2)
     {
         std::string byte = hex.substr(i,2);
-        char chr = (char) (int)strtol(byte.c_str(), null, 16);
-        newString.push_back(chr);
+        char chr = (char) (int)strtol(byte.c_str(), 0, 16);
+        o.push_back(chr);
     }
+    std::cout << o;
     return 0;
 }
