d974b01c35d3dc25b84808e6a053770f8cb2606f
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-09-20 19:32:45 +0800

    Update XHex_decoder.cpp

--- XHex_decoder.cpp
+++ XHex_decoder.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
 
+int main()
+{
+    std::cout << "input hex value: ";
+    std::string hex;
+    std::cin >> hex;
+    int len = hex.length();
+    std::string o;
+    for(int i=0; i< len; i+=2)
+    {
+        std::string byte = hex.substr(i,2);
+        char chr = (char) (int)strtol(byte.c_str(), 0, 16);
+        o.push_back(chr);
+    }
+    std::ofstream file("output.txt");
+    file << o;
+    file.close();
+    std::cout << "Convert success! Read output.txt for actual output." << std::endl;
+    return 0;
+}
